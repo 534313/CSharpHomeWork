7701d1b460a6f5904ea22a603257a2f13f40123f
534313 <61299627+534313@users.noreply.github.com>
2020-03-05 20:47:32 +0800

    Create Program.cs

---
+++ 输出素数因子/输出素数因子/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 输出素数因子
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int num;
+			Console.WriteLine("Please enter the number you want to operate:");
+			if (!Int32.TryParse(Console.ReadLine(), out num))
+			{
+				Console.WriteLine("The number you enter is illegal.");
+			}
+			else 
+			{
+				for (int i = 2; i <= num;) 
+				{
+					if (num % i == 0)
+					{
+						Console.Write(i + ",");
+						num /= i;
+					}
+					else 
+					{
+						i++;
+					}
+				}
+			}
+		}
+	}
+}
