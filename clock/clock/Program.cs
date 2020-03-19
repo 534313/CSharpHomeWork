41971c72f42406b1cc3d8ea5169dd0279c48c54c
534313 <61299627+534313@users.noreply.github.com>
2020-03-20 00:40:57 +0800

    clock

---
+++ clock/clock/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clock
+{
+	public delegate void ClockHandle();
+	
+	public class time_
+	{
+		public event ClockHandle alarm_;
+		public event ClockHandle run_;
+		private int NowTime;
+		public void NowCondition(int AlarmTime )
+		{
+			int second;
+			for (second = 0; second <= 60; second++)
+			{
+				System.Threading.Thread.Sleep(1000);
+				Console.Write("the time is " + second+",");
+				if (second % AlarmTime == 0)
+				{
+					alarm_();
+;				}
+				else
+				{
+					run_();
+				}
+			}
+		}
+	}
+	public class form
+	{
+		public time_ _time = new time_();
+		public form()
+			{
+			_time.alarm_ += new ClockHandle(DoAlarm);
+			_time.run_ += new ClockHandle(DoRun);
+			}
+		public void DoAlarm()
+		{
+			Console.WriteLine("the clock is alarming");
+		}
+		public void DoRun()
+		{
+			Console.WriteLine("the clock is running");
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			form form1 = new form();
+			form1._time.NowCondition(12);
+		}
+	}
+}
