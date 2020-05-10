01abf1122cb42c66bec237bfe25f53f2ff99eafa
534313 <61299627+534313@users.noreply.github.com>
2020-05-10 16:54:46 +0800

    爬虫

---
+++ 爬虫/爬虫/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace 爬虫
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.label2 = new System.Windows.Forms.Label();
+			this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.flowLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("宋体", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.label1.Location = new System.Drawing.Point(30, 25);
+			this.label1.Name = "label1";
+			this.label1.Padding = new System.Windows.Forms.Padding(4, 4, 0, 0);
+			this.label1.Size = new System.Drawing.Size(108, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "起始链接：";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.textBox1.Location = new System.Drawing.Point(145, 29);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(348, 25);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(500, 28);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(103, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "开始爬行";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.label1);
+			this.flowLayoutPanel1.Controls.Add(this.textBox1);
+			this.flowLayoutPanel1.Controls.Add(this.button1);
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(-8, 2);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(27, 25, 0, 0);
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(796, 98);
+			this.flowLayoutPanel1.TabIndex = 4;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Index,
+            this.URL,
+            this.Status});
+			this.dataGridView1.Location = new System.Drawing.Point(6, 142);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersWidth = 51;
+			this.dataGridView1.RowTemplate.Height = 27;
+			this.dataGridView1.Size = new System.Drawing.Size(782, 296);
+			this.dataGridView1.TabIndex = 3;
+			this.dataGridView1.VirtualMode = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(23, 113);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(0, 15);
+			this.label2.TabIndex = 3;
+			// 
+			// Index
+			// 
+			this.Index.DataPropertyName = "Index";
+			this.Index.HeaderText = "序号";
+			this.Index.MinimumWidth = 6;
+			this.Index.Name = "Index";
+			this.Index.Width = 125;
+			// 
+			// URL
+			// 
+			this.URL.DataPropertyName = "URL";
+			this.URL.HeaderText = "链接";
+			this.URL.MinimumWidth = 6;
+			this.URL.Name = "URL";
+			this.URL.Width = 400;
+			// 
+			// Status
+			// 
+			this.Status.DataPropertyName = "Status";
+			this.Status.HeaderText = "状态";
+			this.Status.MinimumWidth = 6;
+			this.Status.Name = "Status";
+			this.Status.Width = 125;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.label2);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.flowLayoutPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Index;
+		private System.Windows.Forms.DataGridViewTextBoxColumn URL;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+	}
+}
+
---
+++ 爬虫/爬虫/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Text.RegularExpressions;
+namespace 爬虫
+{
+	public partial class Form1 : Form
+	{
+		BindingSource bindingSource1 = new BindingSource();
+		SimpleCrawler myCrawler = new SimpleCrawler();
+		Thread thread = null;
+		public Form1()
+		{
+			InitializeComponent();
+			dataGridView1.DataSource = bindingSource1;
+			
+			textBox1.Text = "http://www.cnblogs.com/dstang2000/";
+			myCrawler.PageDownloaded += Crawler_PageDownloaded;
+			myCrawler.CrawlerStopped += Crawler_CrawlerStopped;
+		}
+		private void Crawler_CrawlerStopped(SimpleCrawler obj)
+		{
+			Action action = () => label2.Text = "爬虫已停止";
+			if (this.InvokeRequired)
+			{
+				this.Invoke(action);
+			}
+			else
+			{
+				action();
+			}
+		}
+
+		private void Crawler_PageDownloaded(SimpleCrawler crawler, string url, string info)
+		{
+			var pageInfo = new { Index =bindingSource1.Count + 1, URL = url, Status = info };
+			Action action = () => { bindingSource1.Add(pageInfo); };
+			if (this.InvokeRequired)
+			{
+				this.Invoke(action);
+			}
+			else
+			{
+				action();
+			}
+		}
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			bindingSource1.Clear();
+			myCrawler.StartURL = textBox1.Text;
+
+			Match match = Regex.Match(myCrawler.StartURL, SimpleCrawler.urlParseRegex);
+			if (match.Length == 0) return;
+			string host = match.Groups["host"].Value;
+			myCrawler.HostFilter = "^" + host + "$";
+			myCrawler.FileFilter = ".html?$";
+
+			if (thread != null)
+			{
+				thread.Abort();
+			}
+			thread = new Thread(myCrawler.Start);
+			thread.Start();
+
+
+		}
+		
+	}
+}
